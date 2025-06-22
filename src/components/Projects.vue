d59117c3589702e884c3021fee076670d4dfc012
tlshudson <hudsonteles00@gmail.com>
2025-06-22 02:00:34 -0300

    atualização projeto - projectsGit

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -29,19 +29,19 @@ export default {
         {
           title: 'Controle de Estoque - Em Andamento',
           description: 'Interface frontend para o sistema de controle de estoque SEG Estoque, desenvolvida em Vue 3 com Vite.',
-          cover: 'public/sga.png',
+          cover: '/sga.png',
           link: 'https://github.com/tlshudson/SGA-ESTOQUE'
         },
         {
           title: 'RoyalPark - Finalizado',
           description: 'Interface frontend para o sistema de controle de estoque SEG Estoque, desenvolvida em Vue 3 com Vite.',
-          cover: 'public/royalpark.png',
+          cover: '/royalpark.png',
           link: 'https://github.com/tlshudson/royal_park'
         },
         {
           title: 'Controle de Pontos - Finalizado',
           description: 'Projeto construído em conjunto com Udemy.com/Cod3r-cursos. Tem como objetivo automatizar os batimentos de ponto dos funcionários de uma empresa.',
-          cover: 'public/innout.png',
+          cover: '/innout.png',
           link: 'https://github.com/tlshudson/innout2'
         }
       ]
